e267241dc36fa641bb8da377ae88f7462463f797
Dragos Iordache <dragosh.iordache@yahoo.com>
2020-11-24 20:21:22 +0200

    8;20

--- M02S01/ex04/app.js
+++ M02S01/ex04/app.js
@@ -24,6 +24,49 @@ class Car {
   wheelBack = document.createElement('div');
   hubCapFront = document.createElement('div');
   hubCapBack = document.createElement('div');
+  hazardInterval = 500;
+
+  move(left = 0, top = 0) {
+    left = parseInt(left);
+    top = parseInt(top);
+
+    if (isNaN(left) || isNaN(top)) {
+      left = 0;
+      top = 0;
+    }
+
+    this.frame.style.left = `${left}px`;
+    this.frame.style.top = `${top}px`;
+  }
+
+  changeColor(object, color = '#000') {
+    if (!this.hasOwnProperty(object) || object.nodeName === undefined) {
+      console.warn(`Nu am gasit obiectul ${object} pe aceasta instanta.`);
+
+      return;
+    }
+
+    // object bracket notation
+    this[object].style.backgroundColor = color;
+  }
+
+  toggleHazards() {
+    if (this.intervalId === undefined) {
+      this.intervalId = setInterval(() => {
+        this.toggleLights();
+      }, this.hazardInterval);
+    } else {
+      clearInterval(this.intervalId);
+      this.lightFront.classList.remove('light--on');
+      this.lightBack.classList.remove('light--on');
+      delete this.intervalId;
+    }
+  }
+
+  toggleLights() {
+    this.lightFront.classList.toggle('light--on');
+    this.lightBack.classList.toggle('light--on');
+  }
 
   render() {
     this.frame.classList.add('frame');
