4e85eb5c6ad3eaec7ca6b0965b6f80b4d22b6abe
Dragos Iordache <dragosh.iordache@yahoo.com>
2020-11-26 21:45:08 +0200

    9:45

--- M02S02/ex02/app.js
+++ M02S02/ex02/app.js
@@ -1,3 +1,4 @@
+const formId = 'personForm';
 const createSkillUl = () => {
   const ulId = 'skills-list';
   let $ul = $(`#${ulId}`);
@@ -7,7 +8,7 @@ const createSkillUl = () => {
       id: ulId,
     });
 
-    $('#personForm').after($ul);
+    $(`#${formId}`).after($ul);
 
     $ul.on('click', 'button', (event) => {
       const $element = $(event.currentTarget);
@@ -17,6 +18,21 @@ const createSkillUl = () => {
   }
 
   return $ul;
+};
+
+const createPersonDetails = () => {
+  const detailsId = 'person-details';
+  let $p = $(`#${detailsId}`);
+
+  if ($p.length < 1) {
+    $p = $('<p>', {
+      id: detailsId,
+    });
+
+    $(`#${formId}`).after($p);
+  }
+
+  return $p;
 };
 
 $(document).ready(() => {
@@ -36,5 +52,28 @@ $(document).ready(() => {
     $skillsUl.append($skillLi);
 
     $skillInput.val('');
+  });
+
+  // function version:
+  $(`#${formId}`).on('submit', function (event) {
+    let $form = $(this); // <-- function version this = dom element
+    const data = $form.serializeArray();
+    const desiredKeys = ['name', 'surname', 'age'];
+
+    const userData = data.filter((key) => {
+      if (desiredKeys.includes(key.name)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    $personDetails = createPersonDetails();
+    let message = `
+      Numele meu este ${userData[0].value} ${userData[1].value} si am ${userData[2].value} ani.
+    `;
+    $personDetails.text(message);
+
+    event.preventDefault();
   });
 });
